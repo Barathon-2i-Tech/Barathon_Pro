b8bc4db1ad6e03eebab1a1162d632a02dcc99f82
Pella0 <67748582+Pella0@users.noreply.github.com>
2022-11-28 16:44:02 +0100

    fix(Router): header for welcome page and ReactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
 import Welcome from './Page/Welcome.jsx';
+import {Route, Routes} from "react-router-dom"; 
+
+
+/*<Route path="/register" element={<Register />}></Route>*/
 
 function App() {
     return (
+        
         <div className="App">
-            <Welcome />
+            <Routes>
+                <Route path="/" element={<Welcome />}></Route>
+                <Route path="/register" element={<h1>Register Pro</h1>}></Route>
+                <Route path="/subscribe" element={<h1>Inscription</h1>}></Route>
+            </Routes>
+            
         </div>
+        
     );
 }
 
