db0b599b6673a5cddea7630f43a8288f1148ce8d
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-01-06 10:30:09 +0100

    feat(dashboard): fix dashboard

--- src/Components/CommonComponents/NavigationPro/AppBar.jsx
+++ src/Components/CommonComponents/NavigationPro/AppBar.jsx
@@ -17,108 +17,108 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../Hooks/useAuth';
 
 export const AppBar = ({ pages }) => {
-const [anchorElNav, setAnchorElNav] = React.useState(null);
-const navigate = useNavigate();
-const { user, logout } = useAuth();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const navigate = useNavigate();
+    const { user, logout } = useAuth();
 
-const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-};
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-const handleCloseNavMenu = (path) => {
-    setAnchorElNav(null);
-    if (path) {
-        navigate(path);
-    }
-};
+    const handleCloseNavMenu = (path) => {
+        setAnchorElNav(null);
+        if (path) {
+            navigate(path);
+        }
+    };
 
-return (
-    <MuiAppBar position="static">
-        <Container maxWidth="xl">
-            <Toolbar disableGutters>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="div"
-                    sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                >
-                    Barathon
-                </Typography>
-
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                        color="inherit"
+    return (
+        <MuiAppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
                     >
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
+                        Barathon
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages?.map((page) => (
+                                <MenuItem
+                                    key={page.label}
+                                    onClick={() => handleCloseNavMenu(page.path)}
+                                >
+                                    <Typography textAlign="center">{page.label}</Typography>
+                                </MenuItem>
+                            ))}
+                            {!!user && (
+                                <MenuItem key={'logout'} onClick={logout}>
+                                    <Typography textAlign="center">Logout</Typography>
+                                </MenuItem>
+                            )}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
                     >
+                        Barathon
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         {pages?.map((page) => (
-                            <MenuItem
+                            <Button
                                 key={page.label}
                                 onClick={() => handleCloseNavMenu(page.path)}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
                             >
-                                <Typography textAlign="center">{page.label}</Typography>
-                            </MenuItem>
+                                {page.label}
+                            </Button>
                         ))}
                         {!!user && (
-                            <MenuItem key={'logout'} onClick={logout}>
-                                <Typography textAlign="center">Logout</Typography>
-                            </MenuItem>
+                            <Button
+                                key={'logout'}
+                                onClick={logout}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {'logout'}
+                            </Button>
                         )}
-                    </Menu>
-                </Box>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="div"
-                    sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                >
-                    Barathon
-                </Typography>
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                    {pages?.map((page) => (
-                        <Button
-                            key={page.label}
-                            onClick={() => handleCloseNavMenu(page.path)}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {page.label}
-                        </Button>
-                    ))}
-                    {!!user && (
-                        <Button
-                            key={'logout'}
-                            onClick={logout}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {'logout'}
-                        </Button>
-                    )}
-                </Box>
-            </Toolbar>
-        </Container>
-    </MuiAppBar>
-);
+                    </Box>
+                </Toolbar>
+            </Container>
+        </MuiAppBar>
+    );
 };
--- src/Page/Professional/Dashboard.jsx
+++ src/Page/Professional/Dashboard.jsx
@@ -1,12 +1,7 @@
-
-
-
 export default function Dashboard() {
     return (
         <>
             <div className="relative items-top sm:pt-0 overflow-hidden">
-                
-               
                 <div className="relative">
                     <div className="container mx-auto max-w-screen-xl grid md:grid-cols-2">
                         <div className="bg-gray-500 h-12"></div>
@@ -19,10 +14,9 @@ export default function Dashboard() {
                         <div className="bg-gray-500 h-12"></div>
                     </div>
                     <section className=" container mx-auto max-w-screen-xl py-20 lg:items-center grid grid-cols-1 md:grid-cols-3 gap-10 px-4">
-                    coucou
+                        coucou
                     </section>
                 </div>
-                
             </div>
         </>
     );
