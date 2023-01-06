6ba379f40b5dece6fa7287d3eb8ff162720cc123
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-01-06 10:29:12 +0100

    feat(dashboard): test to dashboard init

--- src/Page/Professional/Dashboard.jsx
+++ src/Page/Professional/Dashboard.jsx
@@ -1,5 +1,29 @@
-function Dashboard() {
-    return <div>Dashboard</div>;
-}
 
-export default Dashboard;
+
+
+export default function Dashboard() {
+    return (
+        <>
+            <div className="relative items-top sm:pt-0 overflow-hidden">
+                
+               
+                <div className="relative">
+                    <div className="container mx-auto max-w-screen-xl grid md:grid-cols-2">
+                        <div className="bg-gray-500 h-12"></div>
+                        <div className="bg-gray-200 h-12"></div>
+                        <div className="bg-gray-200 h-12"></div>
+                        <div className="bg-gray-500 h-12"></div>
+                        <div className="bg-gray-500 h-12"></div>
+                        <div className="bg-gray-200 h-12"></div>
+                        <div className="bg-gray-200 h-12"></div>
+                        <div className="bg-gray-500 h-12"></div>
+                    </div>
+                    <section className=" container mx-auto max-w-screen-xl py-20 lg:items-center grid grid-cols-1 md:grid-cols-3 gap-10 px-4">
+                    coucou
+                    </section>
+                </div>
+                
+            </div>
+        </>
+    );
+}
