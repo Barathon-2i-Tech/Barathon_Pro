41b67cac15d04909ca7d7b5337f0a0a06ee97e42
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-01-26 09:55:03 +0100

    feat(event): test grid

--- src/Page/Professional/Contact.jsx
+++ src/Page/Professional/Contact.jsx
@@ -13,9 +13,6 @@ export default function ContactPage() {
             }}
         >
             <BasicPage title="Contactez nous" icon={<Person />} />
-
-            
-
         </Paper>
     );
 }
--- src/Page/Professional/Event.jsx
+++ src/Page/Professional/Event.jsx
@@ -14,19 +14,16 @@ export default function EventPage() {
         >
             <BasicPage title="Event Page" icon={<Person />} />
             <section className="relative z-10">
-            <div className="container mx-auto max-w-screen-xl sm:py-11 md:py-20 lg:items-center grid grid-cols-6 md:grid-cols-6 gap-10 px-4">
-                
+                <div className="container mx-auto max-w-screen-xl sm:py-11 md:py-20 lg:items-center grid grid-cols-6 md:grid-cols-6 gap-10 px-4">
                     <div className="XXX__card flex justify-center flex-wrap">
-                        
                         <div className="XXX__content">
                             <div className="pb-22 w-full font-bold text-center">
                                 TEST Niv DESCRIPTION
                             </div>
                         </div>
                     </div>
-               
-            </div>
-        </section>
+                </div>
+            </section>
         </Paper>
     );
 }
