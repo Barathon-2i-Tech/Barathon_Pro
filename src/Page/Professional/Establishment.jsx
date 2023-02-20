0c8dd42d8445a8f84f5b27c395a9175e0596bd65
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-02-20 14:42:52 +0100

    feat(establishment): func get and delete

--- src/Page/Professional/Establishment.jsx
+++ src/Page/Professional/Establishment.jsx
@@ -10,60 +10,55 @@ import { ButtonLink } from '../../Components/CommonComponents/ButtonLink';
 import { ButtonDelete } from '../../Components/CommonComponents/ButtonDelete';
 import '../../css/Professional/Establishment.css';
 import Axios from '../../utils/axiosUrl';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function EstablishmentPage() {
-
-   const [establishments, setEstablishments] = useState([]);
+    const [establishments, setEstablishments] = useState([]);
 
     const getEstablishments = () => {
         Axios.api
-        .get(`/pro/1/establishment`, 
-        {
-            headers: {
-                accept: 'application/vnd.api+json',
-                'Content-Type': 'application/vnd.api+json',
-                "Authorization": "Bearer " + "12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc",
-            },
-        },
-        )
-        .then((response) => {
-            setEstablishments(response.data.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+            .get(`/pro/1/establishment`, {
+                headers: {
+                    accept: 'application/vnd.api+json',
+                    'Content-Type': 'application/vnd.api+json',
+                    Authorization: 'Bearer ' + '12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc',
+                },
+            })
+            .then((response) => {
+                setEstablishments(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     const deleteEstablishment = (id) => {
         Axios.api
-          .delete(`/pro/1/establishment/${id}/delete`, {
-            headers: {
-              accept: 'application/vnd.api+json',
-              'Content-Type': 'application/vnd.api+json',
-              Authorization: 'Bearer ' + '12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc',
-            },
-          })
-          .then(() => {
-            console.log('bien effacé');
-            // Supprime l'établissement correspondant de la liste
-            setEstablishments((prevEstablishments) =>
-              prevEstablishments.filter(
-                (establishment) => establishment.establishment_id !== id
-              )
-            );
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+            .delete(`/pro/1/establishment/${id}/delete`, {
+                headers: {
+                    accept: 'application/vnd.api+json',
+                    'Content-Type': 'application/vnd.api+json',
+                    Authorization: 'Bearer ' + '12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc',
+                },
+            })
+            .then(() => {
+                console.log('bien effacé');
+                // Supprime l'établissement correspondant de la liste
+                setEstablishments((prevEstablishments) =>
+                    prevEstablishments.filter(
+                        (establishment) => establishment.establishment_id !== id,
+                    ),
+                );
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     useEffect(() => {
         getEstablishments();
-      }, []);
+    }, []);
 
-
-    
     return (
         <Paper
             sx={{
@@ -75,58 +70,66 @@ export default function EstablishmentPage() {
             }}
         >
             <BasicPage title="Modifier mon etablissement" icon={<BusinessIcon />} />
-            
-            {establishments.map(establishment => (
-            <section key={establishment.establishment_id} className="container relative sm:pt-6 md:pt-11 px-4 z-10">
-                <div className="flex swipe items-center justify-between">
-                    <div className=" text-sm flex mr-3">
-                        <div className="min-100px-min">
-                            <img
-                                className=""
-                                src={establishment.logo}
-                                alt=""
-                                width="100"
-                                height="100"
-                            />
-                        </div>
-                        <div className="pl-3">
-                            <div className="font-bold">{establishment.trade_name}</div>
-                            <div className="">adresse test test test</div>
-                            <div className="">69000</div>
-                        </div>
-                    </div>
-                    <ButtonLink
-                        link="/pro/establishmentForm"
-                        allClass="swipe-item min-100px-min min-w-button-establishement text-center flex align-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3 max-w-xs"
-                        text="Modifier"
-                        icon={<EditIcon />}
-                    />
-                    <ButtonLink
-                        link="/"
-                        allClass="swipe-item min-100px-min min-w-button-establishement text-center flex align-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3"
-                        text={establishment.website}
-                        icon={<LanguageIcon />}
-                    />
 
-                    <div className="swipe-item text-center min-100px-min min-w-button-establishement flex items-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3">
-                        <div className="w-full">
-                            <PhoneIcon />
+            {establishments.map((establishment) => (
+                <section
+                    key={establishment.establishment_id}
+                    className="container relative sm:pt-6 md:pt-11 px-4 z-10"
+                >
+                    <div className="flex swipe items-center justify-between">
+                        <div className=" text-sm flex mr-3">
+                            <div className="min-100px-min">
+                                <img
+                                    className=""
+                                    src={establishment.logo}
+                                    alt=""
+                                    width="100"
+                                    height="100"
+                                />
+                            </div>
+                            <div className="pl-3">
+                                <div className="font-bold">{establishment.trade_name}</div>
+                                <div className="">adresse test test test</div>
+                                <div className="">69000</div>
+                            </div>
                         </div>
-                        <div className="w-full font-bold">{establishment.phone}</div>
-                    </div>
-                    <div className="swipe-item text-center min-100px-min min-w-button-establishement flex items-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3">
-                        <div className="w-full">
-                            <BookmarkBorderIcon />
+                        <ButtonLink
+                            link="/pro/establishmentForm"
+                            allClass="swipe-item min-100px-min min-w-button-establishement text-center flex align-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3 max-w-xs"
+                            text="Modifier"
+                            icon={<EditIcon />}
+                        />
+                        <ButtonLink
+                            link="/"
+                            allClass="swipe-item min-100px-min min-w-button-establishement text-center flex align-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3"
+                            text={establishment.website}
+                            icon={<LanguageIcon />}
+                        />
+
+                        <div className="swipe-item text-center min-100px-min min-w-button-establishement flex items-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3">
+                            <div className="w-full">
+                                <PhoneIcon />
+                            </div>
+                            <div className="w-full font-bold">{establishment.phone}</div>
                         </div>
-                        <div className="w-full font-bold">bar à vin</div>
+                        <div className="swipe-item text-center min-100px-min min-w-button-establishement flex items-center justify-center flex-wrap card-shadow p-2.5 my-1 mr-3">
+                            <div className="w-full">
+                                <BookmarkBorderIcon />
+                            </div>
+                            <div className="w-full font-bold">bar à vin</div>
+                        </div>
+
+                        <ButtonDelete
+                            functionDelete={() =>
+                                deleteEstablishment(establishment.establishment_id)
+                            }
+                            allClass="swipe-item w-fit m-2  min-100px-min text-white bg-red-700 hover:border-solid hover:border-white-900 hover:border-2 pt-2 pb-2 pr-4 pl-4 rounded-lg"
+                        />
                     </div>
 
-                    <ButtonDelete functionDelete={() => deleteEstablishment(establishment.establishment_id)} allClass="swipe-item w-fit m-2  min-100px-min text-white bg-red-700 hover:border-solid hover:border-white-900 hover:border-2 pt-2 pb-2 pr-4 pl-4 rounded-lg" />
-                </div>
-
-                <Divider sx={{ my: 3 }} />
-            </section>
-            ))} 
+                    <Divider sx={{ my: 3 }} />
+                </section>
+            ))}
         </Paper>
     );
 }
--- src/Page/Professional/EstablishmentForm.jsx
+++ src/Page/Professional/EstablishmentForm.jsx
@@ -10,41 +10,36 @@ import Paper from '@mui/material/Paper';
 // import { ButtonDelete } from '../../Components/CommonComponents/ButtonDelete';
 import '../../css/Professional/Establishment.css';
 import Axios from '../../utils/axiosUrl';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function EstablishmentFormPage() {
-
-   const [establishments, setEstablishments] = useState([]);
+    const [establishments, setEstablishments] = useState([]);
 
     const getEstablishments = () => {
         Axios.api
-        .get(`/pro/1/establishment`, 
-        {
-            headers: {
-                accept: 'application/vnd.api+json',
-                'Content-Type': 'application/vnd.api+json',
-                "Authorization": "Bearer " + "12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc",
-            },
-        },
-        )
-        .then((response) => {
-            //console.log(response.data.data + "ceci est le premier");
-            setEstablishments(response.data.data);
-            console.log(establishments);
-            console.log("test");
-        })
-        .catch((error) => {
-          console.log(error);
-          console.log('il y a une erreur')
-        });
+            .get(`/pro/1/establishment`, {
+                headers: {
+                    accept: 'application/vnd.api+json',
+                    'Content-Type': 'application/vnd.api+json',
+                    Authorization: 'Bearer ' + '12|f1fsRWQX5sSyBrn7eImTPtsN22ytqphYquZiprAc',
+                },
+            })
+            .then((response) => {
+                //console.log(response.data.data + "ceci est le premier");
+                setEstablishments(response.data.data);
+                console.log(establishments);
+                console.log('test');
+            })
+            .catch((error) => {
+                console.log(error);
+                console.log('il y a une erreur');
+            });
     };
 
     useEffect(() => {
         getEstablishments();
-      }, []);
+    }, []);
 
-
-    
     return (
         <Paper
             sx={{
@@ -56,14 +51,13 @@ export default function EstablishmentFormPage() {
             }}
         >
             <BasicPage title="Tous mes etablissements" icon={<BusinessIcon />} />
-            
-             
 
-            {establishments.map(establishment => (
-            <section key={establishment.establishment_id} className="container relative sm:pt-6 md:pt-11 px-4 z-10">
-                
-            </section>
-            ))} 
+            {establishments.map((establishment) => (
+                <section
+                    key={establishment.establishment_id}
+                    className="container relative sm:pt-6 md:pt-11 px-4 z-10"
+                ></section>
+            ))}
         </Paper>
     );
 }
