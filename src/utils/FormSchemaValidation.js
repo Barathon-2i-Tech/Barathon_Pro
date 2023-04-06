48aaf9f0f7ab4490a301794202518e41a29c4985
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-04-06 09:37:24 +0200

    feat(establishment): shema validation update and title index

--- src/utils/FormSchemaValidation.js
+++ src/utils/FormSchemaValidation.js
@@ -2,41 +2,47 @@ import * as yup from 'yup';
 
 export const EstablishmentSchemaOpening = () => {
     return yup.object().shape({
-        lundi: yup.string().required('obligatoire'),
-        mardi: yup.string().required('obligatoire'),
-        mercredi: yup.string().required('obligatoire'),
-        jeudi: yup.string().required('obligatoire'),
-        vendredi: yup.string().required('obligatoire'),
-        samedi: yup.string().required('obligatoire'),
-        dimanche: yup.string().required('obligatoire'),
+        lundi: yup.string().required('Veuillez mettre un horaire'),
+        mardi: yup.string().required('Veuillez mettre un horaire'),
+        mercredi: yup.string().required('Veuillez mettre un horaire'),
+        jeudi: yup.string().required('Veuillez mettre un horaire'),
+        vendredi: yup.string().required('Veuillez mettre un horaire'),
+        samedi: yup.string().required('Veuillez mettre un horaire'),
+        dimanche: yup.string().required('Veuillez mettre un horaire'),
     });
 };
 
 export const establishmentSchema = () => {
     return yup.object().shape({
-        trade_name: yup.string().required('obligatoire'),
+        trade_name: yup.string().required('Veuillez remplir le nom de votre établissement'),
         siret: yup
             .string()
-            .required('obligatoire')
+            .required('Ajoutez votre siret')
             .matches(/^[0-9]{14}$/, 'Le siret doit comporter exactement 14 chiffres')
             .test(
                 'is-siret-valid',
                 'Le siret doit comporter exactement 14 chiffres',
                 (val) => val && val.length === 14,
             ),
-        address: yup.string().required('obligatoire'),
-        city: yup.string().required('obligatoire'),
-        postal_code: yup.string().required('obligatoire'),
-        logo: yup.mixed().required('logo is required'),
-        phone: yup.string().required('Téléphone is required'),
-        email: yup.string(),
+        address: yup.string().required('Ajoutez votre adresse'),
+        city: yup.string().required('Ajoutez votre ville'),
+        postal_code: yup
+            .string()
+            .required('Ajoutez votre code postal')
+            .length(5, 'Le code postal doit comporter exactement 5 chiffres'),
+        logo: yup.mixed().required('Ajoutez un logo'),
+        phone: yup
+            .string()
+            .required('Téléphone est requis')
+            .matches(
+                /^\+?\d{1,3}[-.\s]?\d{1,14}[-.\s]?\d{1,14}$/,
+                'Veuillez entrer un numéro de téléphone valide',
+            ),
+        email: yup.string().email('Veuillez entrer une adresse email valide'),
         website: yup.string(),
     });
 };
 
 export const selectCategoriesSchema = () => {
-    return yup.object().shape({
-        // establishment_id: yup.number().required('obligatoire'),
-        // category_id: yup.number().required('obligatoire'),
-    });
+    return yup.object().shape({});
 };
